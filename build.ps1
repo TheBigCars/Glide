3c8e8fd92465c1869c4e8e434c754d540a906831
TheBigCars <carsonpocius@gmail.com>
2026-09-06 22:38:30 -0600

    Compile split controller source

--- build.ps1
+++ build.ps1
@@ -6,6 +6,6 @@ if ($LASTEXITCODE -ne 0) { throw 'Icon generation compilation failed.' }
 & "$PSScriptRoot\GenerateIcon.exe" "$PSScriptRoot\Glide.ico"
 $arguments = @('/nologo', '/target:winexe', '/platform:x64', '/optimize+', '/main:App', "/out:$PSScriptRoot\Glide.exe", "/win32icon:$PSScriptRoot\Glide.ico", "/resource:$PSScriptRoot\MainWindow.xaml,MainWindow.xaml", "/resource:$PSScriptRoot\superlight-2-white.png,superlight-2-white.png")
 $arguments += $references | ForEach-Object { "/reference:$_" }
-$arguments += @("$PSScriptRoot\Probe.cs", "$PSScriptRoot\MouseDevice.cs", "$PSScriptRoot\FirmwareUpdates.cs", "$PSScriptRoot\FirmwareSupport.cs", "$PSScriptRoot\App.cs")
+$arguments += @("$PSScriptRoot\Probe.cs", "$PSScriptRoot\MouseDevice.cs", "$PSScriptRoot\FirmwareUpdates.cs", "$PSScriptRoot\FirmwareSupport.cs", "$PSScriptRoot\Controller.cs", "$PSScriptRoot\Controller.Device.cs", "$PSScriptRoot\App.cs")
 & "$frameworkPath\csc.exe" @arguments
 if ($LASTEXITCODE -ne 0) { throw 'Application compilation failed.' }
